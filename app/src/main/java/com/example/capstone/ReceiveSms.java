9eaa4d8dfa99a2531760f36adb0463a96eb1f279
MarshmallowBox <tjdeh1230@naver.com>
2020-06-24 00:05:50 +0900

    jjim_listview

--- app/src/main/java/com/example/capstone/ReceiveSms.java
+++ app/src/main/java/com/example/capstone/ReceiveSms.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class ReceiveSms extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -29,6 +33,11 @@ public class ReceiveSms extends BroadcastReceiver {
                         MsgBody = MsgBody.replace("원","");
                         String[] data = MsgBody.split(" ");  //스페이스 기준으로 각각 data배열에 저장
 
+                        Date date = new Date();		//컴퓨터 시스템의 시각을 date에 가져온다.
+                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd(E) HH:mm:ss:S");
+
+
+
 
                         for (int j = 0 ; j < data.length ; j++){  //문자파싱
                             System.out.println(j+"번째 배열 : "+data[j]);
@@ -42,7 +51,15 @@ public class ReceiveSms extends BroadcastReceiver {
                                 cardindex = j;
                             }
                         }
-                        if (cardindex != -1 || usemoneyindex != -1 || remainmoneyindex != -1) {
+                        if (!(cardindex == -1 || usemoneyindex == -1 || remainmoneyindex == -1)) {
+                            System.out.println("년 : " + (date.getYear() + 1900));//date의 연도을 가져옴
+                            System.out.println("월 : " + (date.getMonth() + 1));//date의 월을 가져온다
+                            System.out.println("일 : " + date.getDate());	//date의 일을 가져온다.
+                            //System.out.println("요일 : " + date.getDay());	//date의 요일을 가져온다.
+                            System.out.println("시 : " + date.getHours());	//date의 시간을 가져온다.
+                            System.out.println("분 : " + date.getMinutes());	//date의 분을 가져온다.
+                            //System.out.println("초 : " + date.getSeconds());	//date의 초를 가져온다.
+
                             System.out.println("이용카드 : " + data[cardindex]);
                             System.out.println("이용액 : " + data[usemoneyindex]);
                             System.out.println("남은금액 : " + data[remainmoneyindex]);
