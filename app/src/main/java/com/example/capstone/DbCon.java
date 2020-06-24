cf74ddc6f6499d503caf00042bc886358be6bc71
jsh <tngus109404@gmail.com>
2020-06-24 13:20:03 +0900

    search

---
+++ app/src/main/java/com/example/capstone/DbCon.java
@@ -0,0 +1,229 @@
+package com.example.capstone;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DbCon extends AppCompatActivity {
+    private static String TAG = "phpquerytest";
+    private static final String TAG_JSON="webnautes";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_ADDRESS ="address";
+    private static final String TAG_CATEGORY ="category";
+    private static final String TAG_TEL ="tel";
+    private static final String TAG_LATITUDE ="latitude";
+    private static final String TAG_LONGITUDE ="longitude";
+    public static ArrayList<FranchiseDTO> Franchises = new ArrayList<>();
+    static ArrayList<HashMap<String, String>> mArrayList = new ArrayList<>();;
+    static String mJsonString;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public static class Search extends AsyncTask<String, Void, String>{
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "response ------ " + result);
+            if (result == null){
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+        }
+        public static void showResult(){
+            try {
+                System.out.println("과여어어어언?");
+                JSONObject jsonObject = new JSONObject(mJsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+                for(int i=0;i<jsonArray.length();i++){
+                    JSONObject item = jsonArray.getJSONObject(i);
+                    String id = item.getString(TAG_ID);
+                    String name = item.getString(TAG_NAME);
+                    String address = item.getString(TAG_ADDRESS);
+                    String category = item.getString(TAG_CATEGORY);
+                    String tel = item.getString(TAG_TEL);
+                    String latitude = item.getString(TAG_LATITUDE);
+                    String longitude = item.getString(TAG_LONGITUDE);
+                    HashMap<String,String> hashMap = new HashMap<>();
+                    Franchises.add(new FranchiseDTO(Integer.parseInt(id),name,address,category,tel,Double.parseDouble(latitude),Double.parseDouble(longitude)));
+                    mArrayList.add(hashMap);
+                }
+                System.out.println("**************");
+                System.out.println(Franchises);
+                System.out.println("**************");
+            } catch (JSONException e) {
+                Log.d(TAG, "showResult : ", e);
+            }
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String searchKeyword1 = params[0];
+            String searchKeyword2 = params[1];
+            System.out.println(searchKeyword1);
+            System.out.println(searchKeyword2);
+            String serverURL = "http://rtemd.suwon.ac.kr/capstone/search.php";
+            System.out.println(serverURL);
+            String postParameters = "name=" + searchKeyword1 + "&tel=" + searchKeyword2; // tel 쓰면 안되면 변수 새로만들어서 가능, city명 일치한거 하려면 인자 하나더받기
+            System.out.println(postParameters);
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+
+
+    public static class Jjim_ extends AsyncTask<String, Void, String>{
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d(TAG, "response ------ " + result);
+            if (result == null){
+            }
+            else {
+                mJsonString = result;
+                showResult();
+            }
+        }
+        public static void showResult(){
+            try {
+                System.out.println("과여어어어언?");
+                JSONObject jsonObject = new JSONObject(mJsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+                for(int i=0;i<jsonArray.length();i++){
+                    JSONObject item = jsonArray.getJSONObject(i);
+                    String id = item.getString(TAG_ID);
+                    String name = item.getString(TAG_NAME);
+                    String address = item.getString(TAG_ADDRESS);
+                    String category = item.getString(TAG_CATEGORY);
+                    String tel = item.getString(TAG_TEL);
+                    String latitude = item.getString(TAG_LATITUDE);
+                    String longitude = item.getString(TAG_LONGITUDE);
+                    HashMap<String,String> hashMap = new HashMap<>();
+                    Franchises.add(new FranchiseDTO(Integer.parseInt(id),name,address,category,tel,Double.parseDouble(latitude),Double.parseDouble(longitude)));
+                    mArrayList.add(hashMap);
+                }
+                System.out.println("**************");
+                System.out.println(Franchises);
+                System.out.println("**************");
+            } catch (JSONException e) {
+                Log.d(TAG, "showResult : ", e);
+            }
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            String searchKeyword1 = params[0];
+            String searchKeyword2 = params[1];
+            System.out.println(searchKeyword1);
+            System.out.println(searchKeyword2);
+            String serverURL = "http://rtemd.suwon.ac.kr/capstone/search.php";
+            System.out.println(serverURL);
+            String postParameters = "name=" + searchKeyword1 + "&tel=" + searchKeyword2; // tel 쓰면 안되면 변수 새로만들어서 가능, city명 일치한거 하려면 인자 하나더받기
+            System.out.println(postParameters);
+            try {
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+                bufferedReader.close();
+                return sb.toString().trim();
+            } catch (Exception e) {
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+                return null;
+            }
+        }
+    }
+
+
+
+
+}
--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.capstone;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,7 +10,9 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -19,6 +22,7 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.overlay.Marker;
 
@@ -27,23 +31,20 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     public static FragmentManager fragmentManager;
     public static FragmentTransaction fragmentTransaction;
-    public static boolean firsttime = true;
     public static String strNickname, strEmail;
+    public static UI ui;
     static TextView tv_userId, tv_userEmail;
     private SearchView searchView;
     private MenuItem mSearch;
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
-    public static UI ui;
-    public static UI ui2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent2 = new Intent(this, HelpPopupActivity.class);
-        startActivity(intent2);
-        firsttime = true;
+
+
 
 
         //앱바(툴바) 생성부분인데 밑에 이벤트와같이 UI클래스에 같이 합칠수있는지 시도해보기
@@ -69,9 +70,9 @@ public class MainActivity extends AppCompatActivity {
         ui = new UI(this);
         ui.createCategory();
         ui.createNav_Bottom();
-        ui.createNav_Drawer(strNickname, strEmail);
+//        ui.createNav_Drawer(strNickname, strEmail);
         Toast.makeText(this, "이름 : " + strNickname + "이메일 : " + strEmail, Toast.LENGTH_SHORT).show();
-
+        createNav_Drawer(strNickname, strEmail);
 
         //로그인값
         //tv_userId = (TextView)findViewById(R.id.user_id);
@@ -82,12 +83,80 @@ public class MainActivity extends AppCompatActivity {
         //new Intent(getApplicationContext(), LoginInfo.class);
         // startActivity(intent);
     }
+    void createNav_Drawer(String Name, String Email) {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+//        TextView tv_userId = (TextView) context.findViewById(R.id.user_id);
+//        tv_userId.setText(name);
+        // xml 파일에서 넣어놨던 header 선언
+        // header에 있는 리소스 가져오기
+        //로그인시 유저의 아이디값 로그인인텐트에서 받아와 이름변경
+        final TextView textView = navigationView.getHeaderView(0).findViewById(R.id.user_id);
+        textView.setText(Name);
+        final TextView textView1 = navigationView.getHeaderView(0).findViewById(R.id.user_info);
+        textView1.setText(Email);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(false);
+                switch (item.getItemId()) {
+                    case R.id.login:
+                        Toast.makeText(MainActivity.this, "로그인", Toast.LENGTH_SHORT).show();
+                        ui.show();
+                        break;
+                    case R.id.logout:
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("회원정보");
+                        builder.setMessage("회원정보를 보시겠습니까?");
+                        builder.setPositiveButton("예",
 
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent();
+                                        intent.putExtra("logout_user", textView.getText().toString() + "님이 로그아웃 하셨습니다.");
+                                        MainActivity.this.setResult(LoginActivity.LOG_OUT_FLAG = 1, intent);
+                                        MainActivity.this.finish();
+                                    }
+                                });
+                        builder.setNegativeButton("아니오",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+//                                        Toast.makeText(getApplicationContext(),"아니오를 선택했습니다.",Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                        builder.show();
+
+                        break;
+                    case R.id.user_info:
+//                        AlertDialog.Builder builder1 = new AlertDialog.Builder(context);
+//                        LayoutInflater inflater = context.getLayoutInflater();
+//                        View view1 = inflater.inflate(R.layout.user_data,null);
+//                        builder1.setView(view1);
+//                        final AlertDialog dialog1 = builder1.create();
+//                        dialog1.show();
+
+                        break;
+                    case R.id.info:
+                        drawerLayout.closeDrawers();
+                        Intent intent2 = new Intent(getApplicationContext(), HelpPopupActivity.class);
+                        startActivity(intent2);
+
+                        break;
+                    case R.id.preferences:
+                        Toast.makeText(MainActivity.this, "설정", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+    }
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:// 왼쪽 상단 버튼 눌렀을 때
 //                Toast.makeText(MainActivity.this, "햄버거", Toast.LENGTH_SHORT).show();
                 drawerLayout.openDrawer(GravityCompat.START);
+
                 return true;
             case R.id.search_button:
 //                Toast.makeText(MainActivity.this, item.getItemId() + ": search_button", Toast.LENGTH_SHORT).show();
@@ -112,8 +181,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 searchView.clearFocus();
-                Toast.makeText(MainActivity.this, s+"", Toast.LENGTH_SHORT).show();
-
+                System.out.println(s);
+                DbCon.Search Search = new DbCon.Search();
+                Search.execute(s,"1");//인자로 city 스트링 보내면 해당 도시만 출력가능
                 return true;
             }
 
@@ -122,7 +192,11 @@ public class MainActivity extends AppCompatActivity {
                 //입력할때마다 이벤트
                 //여기다 자동완성 넣으면 갸꿀
 
-
+                System.out.println(s);
+                DbCon.Search Search = new DbCon.Search();
+                Search.execute(s,"2");//인자로 city 스트링 보내면 해당 도시만 출력가능
+                System.out.println("잉기모링");
+                System.out.println(DbCon.Franchises);
                 return false;
             }
         });
